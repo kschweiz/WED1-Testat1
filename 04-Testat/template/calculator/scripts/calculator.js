6db18b542c7d92209d792e85430952e575912ccb
Schweizerhof Kevin <Kevin.Schweizerhof@ferag.com>
2019-05-09 16:32:07 +0200

    Calculator JS begonnen

--- 04-Testat/template/calculator/scripts/calculator.js
+++ 04-Testat/template/calculator/scripts/calculator.js
@@ -1,10 +1,85 @@
 /**
  * core
  */
+var keynumbers = document.getElementById("keyboard");
+var number1 = "";
+var number2 = "";
+var display = "";
+var operation3 = "";
+
 class Calculator {
     constructor() {
 
     }
+}
+
+function buttonNumClickHandler(){
+    var content = this.value;
+    
+    if(operation3 === ""){
+        number1 += content;
+        display += content;
+        writeInput(display);
+    }else{
+        number2 += content;
+        display += content;
+        writeInput(display);
+    }
+}
+
+function buttonOpClickHandler(){
+    var content = this.value;
+    if(operation3 === "" && number1 !== ""){
+        operation3 = content;
+        display += content;
+        writeInput(display);
+    }
+}
+
+function buttonCommandHandler(){
+    if(operation3 === "/" && parseFloat(number2, 10) === 0){
+        buttonClearHandler();
+        writeOutput("Divsion by Zero not allowed!")
+    }else{
+        var result = "";
+        if(operation3 === "+"){
+            result = parseFloat(number1, 10) + parseFloat(number2, 10);
+        }else if(operation3 === "-"){
+            result = parseFloat(number1, 10) - parseFloat(number2, 10);
+        }else if(operation3 === "*"){
+            result = parseFloat(number1, 10) * parseFloat(number2, 10);
+        }else if(operation3 === "/"){
+            result = parseFloat(number1, 10) / parseFloat(number2, 10);
+        }else{
+            writeOutput("ERROR")
+        }
+    
+        number1 = result;
+        number2 = "";
+        display = result;
+        operation3 = "";
+        writeInput(result);
+        writeOutput(result);
+    }
+
+    
+}
+
+function buttonClearHandler(){
+    number1 = "";
+    number2 = "";
+    display = "";
+    operation3 = "";
+    writeInput("");
+    writeOutput("");
+}
+
+function writeOutput(output){
+    document.getElementById("output").innerHTML = output;
+}
+
+function writeInput(input){
+    document.getElementById("input").innerHTML = input;
 }
 
 
@@ -15,6 +90,17 @@ window.addEventListener('DOMContentLoaded', function() {
     const calculator = new Calculator();
 
 });
+
+
+$(document).on("ready", function() {
+    $("#output").text("Welcome");
+    $(".number").on("click", buttonNumClickHandler);
+    $(".operator").on("click", buttonOpClickHandler);
+    $(".command").on("click", buttonCommandHandler);
+    $(".clear").on("click", buttonClearHandler);
+ });
+
+
 
 
 /**
